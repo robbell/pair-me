32e896796e3b37310971af5fde8976690667b926
Rob Bell <me@rob-bell.net>
2017-03-20 10:21:04 +0000

    Add Slack command parameters to controller

--- PairMe.Api/Controllers/PairController.cs
+++ PairMe.Api/Controllers/PairController.cs
@@ -1,37 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 
 namespace PairMe.Api.Controllers
 {
     [Route("api/[controller]")]
     public class PairController : Controller
     {
-        // POST api/pair
-        [HttpPost]
-        public CommandResponse Post(CommandRequest commandRequest)
+        public PairCommandResponse Post(PairCommandRequest commandRequest)
         {
-            return new CommandResponse { Text = $"{commandRequest.Text} {commandRequest.UserId} {commandRequest.User_Id}  {commandRequest.Username} {commandRequest.Token}" };
+            return new PairCommandResponse
+            {
+                Text = $@"{commandRequest.Token} 
+{commandRequest.TeamId} 
+{commandRequest.TeamDomain} 
+{commandRequest.ChannelId} 
+{commandRequest.ChannelName} 
+{commandRequest.UserId}
+{commandRequest.Username}
+{commandRequest.Command}
+{commandRequest.Text}"
+            };
         }
     }
 
-    public class CommandRequest
+    public class PairCommandRequest
     {
         public string Token { get; set; }
-        [JsonProperty("team_id")]
+        [FromQuery(Name = "team_id")]
         public string TeamId { get; set; }
-        [JsonProperty("channel_id")]
+        [FromQuery(Name = "team_domain")]
+        public string TeamDomain { get; set; }
+        [FromQuery(Name = "channel_id")]
         public string ChannelId { get; set; }
-        [JsonProperty("channel_name")]
+        [FromQuery(Name = "channel_name")]
         public string ChannelName { get; set; }
-        [JsonProperty("user_id")]
+        [FromQuery(Name = "user_id")]
         public string UserId { get; set; }
-        public string User_Id { get; set; }
+        [FromQuery(Name = "user_name")]
         public string Username { get; set; }
         public string Command { get; set; }
         public string Text { get; set; }
     }
 
-    public class CommandResponse
+    public class PairCommandResponse
     {
         public string Text { get; set; }
     }
